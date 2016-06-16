b48c211369a9b3f203bd5911ec96f0ee1f1bbbf8
Dylan Graham <dylan.graham@gmail.com>
2016-06-16 16:40:29 +1000

    latest

--- logcheck.py
+++ logcheck.py
@@ -1,29 +1,44 @@
-import re
 from node import Node
-
-work_dir = '/home/dylan/Downloads/pbs_server_logs/'
-file_name = '20150212'
-node_name = 'comp079'
 
 
 class LogCheck:
     def __init__(self):
-        self.nodes = []
+        self.node_names = []
+        self.nodes = {'system': Node('system')}
+
+    def setup_node_objects(self):
+        num_nodes = [format(x, '03d') for x in range(174)]
+        self.node_names = ["comp" + str(i) for i in num_nodes]
+        for n in self.node_names:
+            self.nodes[n] = Node(n)
+
+    def name_in_line(self, line):
+        for name in self.node_names:
+            if name in line:
+                yield name
 
     def readfile(self, file):
         for line in open(file):
-            if re.search(node_name, line):
+            name_func = self.name_in_line(line)
+            try:
+                name = name_func.__next__()
+            except StopIteration:
+                continue
+            if name:
+                self.nodes[name].make_log_entry(line)
+            else:
+                self.nodes['system'].make_log_entry(line)
 
-                if node_name not in self.nodes:
-                    x = Node(node_name)
-                    x.make_log_entry(line)
-                    self.nodes.append(x)
-                else:
-                    self.nodes.append(line)
+work_dir = '/home/dylan/Downloads/pbs_server_logs/'
+file_name = '20160614'
 
 l = LogCheck()
+l.setup_node_objects()
 l.readfile(work_dir + file_name)
-for node in l.nodes:
-    node.print_log()
 
+# for node in l.nodes:
+#     if l.nodes[node].log:
+#         print(l.nodes[node].name)
+#         l.nodes[node].print_log()
 
+l.nodes['comp147'].print_log()
--- node.py
+++ node.py
@@ -5,9 +5,11 @@ class Node:
         self.name = name
 
     def make_log_entry(self, line):
+        # TODO: make this tuple of tuples
         self.log += tuple(line.split(';'))
 
     def print_log(self):
         for line in self.log:
+            #print(line[0], line[4])
             print(line)
 
