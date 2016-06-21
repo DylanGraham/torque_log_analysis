e0cb7375ea0274bbb511edce49d89b3202113434
Dylan Graham <dylan.graham@gmail.com>
2016-06-21 14:17:24 +1000

    Add to init

--- logcheck.py
+++ logcheck.py
@@ -5,6 +5,7 @@ class LogCheck:
     def __init__(self):
         self.node_names = []
         self.nodes = {'system': Node('system')}
+        self.setup_node_objects()
 
     def setup_node_objects(self):
         num_nodes = [format(x, '03d') for x in range(174)]
@@ -29,7 +30,6 @@ work_dir = '/home/dylan/Downloads/pbs_server_logs/'
 file_name = '20160614'
 
 l = LogCheck()
-l.setup_node_objects()
 l.readfile(work_dir + file_name)
 
 for log in l.nodes['comp147']:
