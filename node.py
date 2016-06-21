4751e11eef27c4896ec8dec6706f61420d1713f8
Dylan Graham <dylan.graham@gmail.com>
2016-06-21 14:32:59 +1000

    Index location

--- node.py
+++ node.py
@@ -2,17 +2,17 @@ class Node:
     def __init__(self, name):
         self.log = []
         self.name = name
-        self.count = -1
 
     def __iter__(self):
+        self.index = -1
         return self
 
     def __next__(self):
-        self.count += 1
-        if self.count >= len(self.log):
+        self.index += 1
+        if self.index >= len(self.log):
             raise StopIteration
         else:
-            return self.log[self.count][0]
+            return self.log[self.index][0]
 
     def make_log_entry(self, line):
         self.log.append(list(line.split(';')))
