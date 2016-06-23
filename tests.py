edd37e19855ea60b333360548dd0fedbef41a6a8
Dylan Graham <dylan.graham@gmail.com>
2016-06-24 00:44:00 +1000

    cleanup

--- tests.py
+++ tests.py
@@ -5,8 +5,8 @@ from logcheck import LogCheck
 class Tests(unittest.TestCase):
     def test_setup_node_objects(self):
         l = LogCheck()
-        self.assertIn('comp001', l.node_names)
-        self.assertIn('comp147', l.node_names)
+        for name in ('comp001', 'comp147'):
+            self.assertIn(name, l.node_names)
 
 if __name__ == '__main__':
     unittest.main()
